f8c69001460121e12d92b90b54081e53cf7dda13
Pavel Garaev <ipagar@icloud.com>
2023-09-12 13:02:42 +0400

    change url to connectionString

--- dbs/config.js
+++ dbs/config.js
@@ -2,7 +2,7 @@ const { Pool } = require("pg");
 const { MongoClient } = require("mongodb");
 
 const postgres = new Pool({
-  url: process.env.DB_URL,
+  connectionString: process.env.DB_URL,
 });
 
 // Connection URL
