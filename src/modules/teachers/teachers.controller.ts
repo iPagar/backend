4dfbfaf514e5ae61bd2edea4375b26243132d5c7
Pavel Garaev <ipagar@icloud.com>
2024-01-18 21:53:26 +0400

    Refactor teacher update logic

--- src/modules/teachers/teachers.controller.ts
+++ src/modules/teachers/teachers.controller.ts
@@ -477,86 +477,88 @@ export class TeachersController {
   // every week
   @Cron("0 0 * * 1")
   async updateTeachers() {
-    const teachers = await getTeachersFromWebsite();
-    const filteredTeachers: {
-      name: string;
-      position: string;
-      qualification: string;
-      subjects: string;
-    }[] = teachers.map((teacher) => ({
-      name: teacher.name,
-      position: teacher.position,
-      qualification: teacher.qualification,
-      subjects: teacher.disciplines,
-    }));
+    if (process.env.UPDATE_TEACHERS === "true") {
+      const teachers = await getTeachersFromWebsite();
+      const filteredTeachers: {
+        name: string;
+        position: string;
+        qualification: string;
+        subjects: string;
+      }[] = teachers.map((teacher) => ({
+        name: teacher.name,
+        position: teacher.position,
+        qualification: teacher.qualification,
+        subjects: teacher.disciplines,
+      }));
 
-    await this.prismaService.$transaction(
-      async (tx) => {
-        const inactive = await tx.teachers.findMany({
-          where: {
-            name: {
-              notIn: filteredTeachers.map((teacher) => teacher.name),
-            },
-          },
-        });
-
-        await tx.teachers.updateMany({
-          where: {
-            id: {
-              in: inactive.map((teacher) => teacher.id),
-            },
-          },
-          data: {
-            status: "inactive",
-          },
-        });
-
-        const active = await tx.teachers.findMany({
-          where: {
-            name: {
-              in: filteredTeachers.map((teacher) => teacher.name),
-            },
-          },
-        });
-
-        const update = filteredTeachers.filter((teacher) =>
-          active.find((activeTeacher) => activeTeacher.name === teacher.name)
-        );
-
-        const updatePromises = update.map(async (teacher) => {
-          await tx.teachers.update({
+      await this.prismaService.$transaction(
+        async (tx) => {
+          const inactive = await tx.teachers.findMany({
             where: {
-              name: teacher.name,
-            },
-            data: {
-              name: teacher.name,
-              position: teacher.position,
-              qualification: teacher.qualification,
-              subjects: teacher.subjects,
-              status: "active",
+              name: {
+                notIn: filteredTeachers.map((teacher) => teacher.name),
+              },
             },
           });
-        });
 
-        await Promise.all(updatePromises);
+          await tx.teachers.updateMany({
+            where: {
+              id: {
+                in: inactive.map((teacher) => teacher.id),
+              },
+            },
+            data: {
+              status: "inactive",
+            },
+          });
 
-        const newTeachers = filteredTeachers.filter(
-          (teacher) =>
-            !active.find(
-              (activeTeacher) => activeTeacher.name === teacher.name
-            ) &&
-            !inactive.find(
-              (inactiveTeacher) => inactiveTeacher.name === teacher.name
-            )
-        );
+          const active = await tx.teachers.findMany({
+            where: {
+              name: {
+                in: filteredTeachers.map((teacher) => teacher.name),
+              },
+            },
+          });
 
-        await tx.teachers.createMany({
-          data: newTeachers,
-        });
-      },
-      {
-        timeout: 50000,
-      }
-    );
+          const update = filteredTeachers.filter((teacher) =>
+            active.find((activeTeacher) => activeTeacher.name === teacher.name)
+          );
+
+          const updatePromises = update.map(async (teacher) => {
+            await tx.teachers.update({
+              where: {
+                name: teacher.name,
+              },
+              data: {
+                name: teacher.name,
+                position: teacher.position,
+                qualification: teacher.qualification,
+                subjects: teacher.subjects,
+                status: "active",
+              },
+            });
+          });
+
+          await Promise.all(updatePromises);
+
+          const newTeachers = filteredTeachers.filter(
+            (teacher) =>
+              !active.find(
+                (activeTeacher) => activeTeacher.name === teacher.name
+              ) &&
+              !inactive.find(
+                (inactiveTeacher) => inactiveTeacher.name === teacher.name
+              )
+          );
+
+          await tx.teachers.createMany({
+            data: newTeachers,
+          });
+        },
+        {
+          timeout: 50000,
+        }
+      );
+    }
   }
 }
