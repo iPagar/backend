c44556893fd1722dc81224529302cf134f325e4d
Pavel Garaev <ipagar@icloud.com>
2024-01-18 23:20:39 +0400

    Add axios-retry package and update getTeacherDetail function

--- src/modules/teachers/teachers.controller.ts
+++ src/modules/teachers/teachers.controller.ts
@@ -18,14 +18,13 @@ import {
   getSchemaPath,
 } from "@nestjs/swagger";
 import { PrismaService } from "../../prisma.service";
-import { getTeachersFromWebsite } from "./teachers.helpers";
+import { getTeacherDetail, getTeachersFromWebsite } from "./teachers.helpers";
 import { Cron } from "@nestjs/schedule";
 import {
   StudentParam,
   UseStudent,
 } from "../../common/decorators/student.decorator";
 import { PutReactionDto } from "./dto/reaction.dto";
-import { getTeacherDetail } from "./utils/get-teacher-details";
 import { PaginationDto } from "../../common/dto/pagination.dto";
 import { ApiPaginationQuery } from "../../common/dto/pagination.dto";
 import {
@@ -475,90 +474,90 @@ export class TeachersController {
   }
 
   // every week
-  @Cron("0 0 * * 1")
+  @Cron("0 0 * * 1", {
+    disabled: process.env.UPDATE_TEACHERS !== "true",
+  })
   async updateTeachers() {
-    if (process.env.UPDATE_TEACHERS === "true") {
-      const teachers = await getTeachersFromWebsite();
-      const filteredTeachers: {
-        name: string;
-        position: string;
-        qualification: string;
-        subjects: string;
-      }[] = teachers.map((teacher) => ({
-        name: teacher.name,
-        position: teacher.position,
-        qualification: teacher.qualification,
-        subjects: teacher.disciplines,
-      }));
+    const teachers = await getTeachersFromWebsite();
+    const filteredTeachers: {
+      name: string;
+      position: string;
+      qualification: string;
+      subjects: string;
+    }[] = teachers.map((teacher) => ({
+      name: teacher.name,
+      position: teacher.position,
+      qualification: teacher.qualification,
+      subjects: teacher.disciplines,
+    }));
 
-      await this.prismaService.$transaction(
-        async (tx) => {
-          const inactive = await tx.teachers.findMany({
-            where: {
-              name: {
-                notIn: filteredTeachers.map((teacher) => teacher.name),
-              },
+    await this.prismaService.$transaction(
+      async (tx) => {
+        const inactive = await tx.teachers.findMany({
+          where: {
+            name: {
+              notIn: filteredTeachers.map((teacher) => teacher.name),
             },
-          });
+          },
+        });
 
-          await tx.teachers.updateMany({
+        await tx.teachers.updateMany({
+          where: {
+            id: {
+              in: inactive.map((teacher) => teacher.id),
+            },
+          },
+          data: {
+            status: "inactive",
+          },
+        });
+
+        const active = await tx.teachers.findMany({
+          where: {
+            name: {
+              in: filteredTeachers.map((teacher) => teacher.name),
+            },
+          },
+        });
+
+        const update = filteredTeachers.filter((teacher) =>
+          active.find((activeTeacher) => activeTeacher.name === teacher.name)
+        );
+
+        const updatePromises = update.map(async (teacher) => {
+          await tx.teachers.update({
             where: {
-              id: {
-                in: inactive.map((teacher) => teacher.id),
-              },
+              name: teacher.name,
             },
             data: {
-              status: "inactive",
+              name: teacher.name,
+              position: teacher.position,
+              qualification: teacher.qualification,
+              subjects: teacher.subjects,
+              status: "active",
             },
           });
+        });
 
-          const active = await tx.teachers.findMany({
-            where: {
-              name: {
-                in: filteredTeachers.map((teacher) => teacher.name),
-              },
-            },
-          });
+        await Promise.all(updatePromises);
 
-          const update = filteredTeachers.filter((teacher) =>
-            active.find((activeTeacher) => activeTeacher.name === teacher.name)
-          );
+        const newTeachers = filteredTeachers.filter(
+          (teacher) =>
+            !active.find(
+              (activeTeacher) => activeTeacher.name === teacher.name
+            ) &&
+            !inactive.find(
+              (inactiveTeacher) => inactiveTeacher.name === teacher.name
+            )
+        );
 
-          const updatePromises = update.map(async (teacher) => {
-            await tx.teachers.update({
-              where: {
-                name: teacher.name,
-              },
-              data: {
-                name: teacher.name,
-                position: teacher.position,
-                qualification: teacher.qualification,
-                subjects: teacher.subjects,
-                status: "active",
-              },
-            });
-          });
-
-          await Promise.all(updatePromises);
-
-          const newTeachers = filteredTeachers.filter(
-            (teacher) =>
-              !active.find(
-                (activeTeacher) => activeTeacher.name === teacher.name
-              ) &&
-              !inactive.find(
-                (inactiveTeacher) => inactiveTeacher.name === teacher.name
-              )
-          );
-
-          await tx.teachers.createMany({
-            data: newTeachers,
-          });
-        },
-        {
-          timeout: 50000,
-        }
-      );
-    }
+        await tx.teachers.createMany({
+          data: newTeachers,
+        });
+      },
+      {
+        timeout: 100000,
+      }
+    );
   }
 }
--- src/modules/teachers/teachers.helpers.ts
+++ src/modules/teachers/teachers.helpers.ts
@@ -12,6 +12,20 @@
 
 import axios from "axios";
 import { z } from "zod";
+import rateLimit from "axios-rate-limit";
+import axiosRetry from "axios-retry";
+
+const teacherAxios = rateLimit(
+  axios.create({
+    baseURL: "https://stankin.ru/api_entry.php",
+    timeout: 900000,
+  }),
+  {
+    maxRequests: 1,
+    perMilliseconds: 1000,
+  }
+);
+axiosRetry(teacherAxios, { retries: 3 });
 
 export type Teacher = {
   name: string;
@@ -59,7 +73,7 @@ const teachersMock: Teacher[] = [
 
 export async function getTeachersFromWebsite(): Promise<Teacher[]> {
   try {
-    const response = await axios.post("https://stankin.ru/api_entry.php", {
+    const response = await teacherAxios.post("", {
       action: "getJSONTable",
       data: { id: 128 },
     });
@@ -96,4 +110,71 @@ export async function getTeachersFromWebsite(): Promise<Teacher[]> {
     console.error(error);
     return [];
   }
+}
+
+export async function getTeacherDetail(name: string) {
+  const path = "https://stankin.ru/api_entry.php";
+
+  const optionsSearchInfo = {
+    body: {
+      action: "search",
+      data: {
+        type: "users",
+        query: name,
+        page: 1,
+        count: 20,
+      },
+    },
+  };
+
+  try {
+    const searchInfoResponse = await teacherAxios.post("", {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(optionsSearchInfo.body),
+    });
+    const infoResponse = searchInfoResponse.data as any;
+
+    if (infoResponse) {
+      if (infoResponse.data.founded.length > 0) {
+        const { id } = infoResponse.data.founded[0];
+        const subdivisionId =
+          infoResponse.data.founded[0].payload.subdivision_id;
+
+        const optionsTeacherInfo = {
+          method: "POST",
+          uri: path,
+          body: {
+            action: "getStuff",
+            data: { subdivision_id: subdivisionId },
+          },
+          json: true,
+        };
+        const teacherInfoResponse = await fetch(path, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(optionsTeacherInfo.body),
+        });
+        const teacherResponse = (await teacherInfoResponse.json()) as any;
+        if (teacherResponse) {
+          const { email, phone, avatar } = teacherResponse.data.filter(
+            (row: any) => row.user.id === id
+          )[0].user;
+
+          return {
+            email: email !== "example@mail.com" ? email : null,
+            phone,
+            avatar,
+          };
+        }
+      }
+    }
+  } catch (e) {
+    return {};
+  }
+
+  return {};
 }
